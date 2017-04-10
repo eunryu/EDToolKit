f7991e1f19499b2ee7eb4688589e4c709b106968
은아월 <instru93@naver.com>
2017-04-10 10:47:58 +0900

    세그 이펙트 추가.

---
+++ EDToolKit/Classes/FadeInOutEffectSegue.swift
@@ -0,0 +1,21 @@
+//
+//  FadeInOutEffectSegue.swift
+//  Pods
+//
+//  Created by 은아월 on 2017. 4. 10..
+//
+//
+
+import Foundation
+import UIKit
+
+class FadeInOutEffectSegue: UIStoryboardSegue {
+    override func perform() {
+        let transition = CATransition()
+        transition.duration = 0.3
+        transition.type = kCATransitionFade
+        
+        self.source.navigationController?.view.layer.add(transition, forKey: kCATransition)
+        self.source.navigationController?.pushViewController(self.destination, animated: false)
+    }
+}
