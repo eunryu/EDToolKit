a9394b728529090b82393d000c0050335808f614
은아월 <instru93@naver.com>
2018-05-25 10:17:52 +0900

    # 변경사항은 다음과 같습니다. - 4.0.1 업데이트 준비
    # 이슈사항은 다음과 같습니다.

--- EDToolKit/Classes/ExtensionKit.swift
+++ EDToolKit/Classes/ExtensionKit.swift
@@ -17,20 +17,4 @@ public extension String{
     func localizedWithComment(_ comment:String) -> String{
         return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: "", comment: comment)
     }
-    func matchPattern(_ patStr:String)->Bool {
-        var isMatch:Bool = false
-        do {
-            let regex = try NSRegularExpression(pattern: patStr, options: [.caseInsensitive])
-            let result = regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(rawValue: UInt(0)), range: NSMakeRange(0, characters.count))
-            
-            if (result != nil)
-            {
-                isMatch = true
-            }
-        }
-        catch {
-            isMatch = false
-        }
-        return isMatch
-    }
 }
